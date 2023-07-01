8d0994c56379a8fcf6fffe050bca5f40c5dec46f
Young Oh <youngoh@MacBook-Pro-7.local>
2023-07-01 11:47:45 -0700

    bruh

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ const widget = document.getElementById('widget');
 
 // Create the input fields and submit button
 const input1 = document.createElement('input');
-input1.placeholder = 'Number of patients scheduled this month';
+input1.placeholder = 'Enter information...';
 widget.appendChild(input1);
 
 const submitButton = document.createElement('button');
 submitButton.textContent = 'Submit';
 widget.appendChild(submitButton);
 
+// Create a container for displaying the result
+const resultContainer = document.createElement('div');
+widget.appendChild(resultContainer);
+
 // Handle submit button click event
 submitButton.addEventListener('click', () => {
   const userInput = input1.value;
 
-  // Call your Python script or perform any desired processing here
-  // For simplicity, let's display the user input in an alert box
-  alert(`You entered: ${userInput}`);
+  // Display the user input in the result container
+  resultContainer.textContent = `You entered: ${userInput}`;
+
+  // Clear the input field
+  input1.value = '';
 });
-
-
-
-
-
